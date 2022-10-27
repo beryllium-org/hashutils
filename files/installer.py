62632c9e58bbfa12f93ba271ee25d9ce53dc7f38
Bill Sideris <bill88t@feline.gr>
2022-10-27 14:58:09 +0300

    installation stuff

--- files/installer.py
+++ files/installer.py
@@ -1,6 +1,36 @@
-print("""
-    This script is run during the first install of the package.
-    
-    You should copy the files you want in the system from here.
-    You should NOT bother with the manifest or the uninstall files from here.
-""")
+# adafruit_hashlib
+ljinux.api.var("argj", f"mkdir &/lib/adafruit_hashlib")
+ljinux.based.command.fpexecc([None, "/bin/mkdir.py"])
+for filee in [
+    "__init__.mpy",
+    "_md5.mpy",
+    "_sha1.mpy",
+    "_sha224.mpy",
+    "_sha256.mpy",
+    "_sha384.mpy",
+    "_sha512.mpy",
+]:
+    ljinux.api.var("argj", f"cp {filee} &/lib/adafruit_hashlib/{filee}")
+    ljinux.based.command.fpexecc([None, "/bin/cp.py"])
+    del filee
+
+# bins
+for filee in [
+    "md5sum.lja",
+    "md5sum.py",
+    "sha1sum.lja",
+    "sha1sum.py",
+    "sha224sum.lja",
+    "sha224sum.py",
+    "sha256sum.lja",
+    "sha256sum.py",
+    "sha384sum.lja",
+    "sha384sum.py",
+    "sha512sum.lja",
+    "sha512sum.py",
+]:
+    ljinux.api.var("argj", f"cp {filee} /bin/{filee}")
+    ljinux.based.command.fpexecc([None, "/bin/cp.py"])
+    del filee
+
+ljinux.api.var("return", "0")
--- files/uninstaller.py
+++ files/uninstaller.py
@@ -1,7 +1,60 @@
-print("""
-    This script is run when a users uninstalls the package.
-    
-    It will not run during an update.
-    Here you should purge all the files you installed with your install script.
-    Do not nuke the system.
-""")
+# adafruit_hashlib
+hl = " &/lib/adafruit_hashlib/"
+ljinux.api.var(
+    "argj",
+    "rm"
+    + hl
+    + "__init__.mpy"
+    + hl
+    + "_md5.mpy"
+    + hl
+    + "_sha1.mpy"
+    + hl
+    + "_sha224.mpy"
+    + hl
+    + "_sha256.mpy"
+    + hl
+    + "_sha384.mpy"
+    + hl
+    + "_sha512.mpy",
+)
+ljinux.based.command.fpexecc([None, "/bin/rm.py"])
+del hl
+
+ljinux.api.var("argj", f"rmdir &/lib/adafruit_hashlib")
+ljinux.based.command.fpexecc([None, "/bin/rmdir.py"])
+
+# bins
+bl = " /bin/"
+ljinux.api.var(
+    "argj",
+    "rm"
+    + bl
+    + "md5sum.lja"
+    + bl
+    + "md5sum.py"
+    + bl
+    + "sha1sum.lja"
+    + bl
+    + "sha1sum.py"
+    + bl
+    + "sha224sum.lja"
+    + bl
+    + "sha224sum.py"
+    + bl
+    + "sha256sum.lja"
+    + bl
+    + "sha256sum.py"
+    + bl
+    + "sha384sum.lja"
+    + bl
+    + "sha384sum.py"
+    + bl
+    + "sha512sum.lja"
+    + bl
+    + "sha512sum.py",
+)
+ljinux.based.command.fpexecc([None, "/bin/rm.py"])
+del bl
+
+ljinux.api.var("return", "0")
