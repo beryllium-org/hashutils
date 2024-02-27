c796edaa1b827b33fa2dd815d2b4707ad8fdb68d
Bill Sideris <bill88t@feline.gr>
2024-02-27 19:56:50 +0200

    Add strapper

---
+++ files/strap.py
@@ -0,0 +1,42 @@
+try:
+    mkdir(path.join(root, "lib/adafruit_hashlib"))
+except FileExistsError:
+    pass
+
+for i in [
+    "__init__.mpy",
+    "_md5.mpy",
+    "_sha1.mpy",
+    "_sha224.mpy",
+    "_sha256.mpy",
+    "_sha384.mpy",
+    "_sha512.mpy",
+]:
+    shutil.copy(i, path.join(root, "lib/adafruit_hashlib", i))
+
+for i in [
+    "crc32.lja",
+    "md5sum.lja",
+    "sha1sum.lja",
+    "sha224sum.lja",
+    "sha256sum.lja",
+    "sha384sum.lja",
+    "sha512sum.lja",
+]:
+    shutil.copy(i, path.join(root, "bin", i))
+
+try:
+    mkdir(path.join(root, "bin/hashutils"))
+except FileExistsError:
+    pass
+
+for i in [
+    "crc32.py",
+    "md5sum.py",
+    "sha1sum.py",
+    "sha224sum.py",
+    "sha256sum.py",
+    "sha384sum.py",
+    "sha512sum.py",
+]:
+    shutil.copy(i, path.join(root, "bin/hashutils", i))
